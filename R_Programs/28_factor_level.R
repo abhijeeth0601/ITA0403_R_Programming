a78e7383161379b76038feba0862fa48bd2ef661
ABHIJEETH <113879749+abhijeeth0601@users.noreply.github.com>
2023-09-06 21:58:05 +0530

    Update 28_factor_level.R

--- R_Programs/28_factor_level.R
+++ R_Programs/28_factor_level.R
@@ -1,5 +1,14 @@
-set.seed(123)  # For reproducibility
-sample_letters <- sample(LETTERS, size = 5)
-sample_factor <- factor(sample_letters)
-factor_levels <- levels(sample_factor)
-print(factor_levels)
+# Set a seed for reproducibility
+set.seed(123)
+
+# Create a random sample of letters from the LETTERS dataset
+random_letters <- sample(LETTERS, 26, replace = TRUE)
+
+# Create a factor from the random sample
+letter_factor <- factor(random_letters)
+
+# Extract the first five levels from the factor
+first_five_levels <- levels(letter_factor)[1:5]
+
+# Print the first five levels
+print(first_five_levels)
