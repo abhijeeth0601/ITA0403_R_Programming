ed9915ba251e0e4b16b839ca4c4baa23d2506d9d
ABHIJEETH <113879749+abhijeeth0601@users.noreply.github.com>
2023-09-06 22:19:45 +0530

    Update 30_confusion_matrix_iris.R

--- R_Programs/30_confusion_matrix_iris.R
+++ R_Programs/30_confusion_matrix_iris.R
@@ -1,20 +1,12 @@
-# Load necessary packages
-if (!require(caret)) {
-  install.packages("caret")
-  library(caret)
-}
-
-if (!require(nnet)) {
-  install.packages("nnet")
-  library(nnet)
-}
+library(caret)
+library(nnet)
 
 data("iris")
 set.seed(123)
 # Split data (80% train, 20% test)
 splitIndex <- createDataPartition(iris$Species, p = 0.8, list = FALSE)
-train_data <- iris[splitIndex, ]
-test_data <- iris[-splitIndex, ]
+train_data <- iris[splitIndex,]
+test_data <- iris[-splitIndex,]
 
 # Train a multinomial logistic regression model and predict
 model <- train(Species ~ Petal.Width + Petal.Length, data = train_data, method = "multinom")
